6f4d62b17cd7920075b7de49561ef7aeca7de0d0
中山颯 <sippu242@gmail.com>
2019-07-10 02:23:24 +0900

    add gitignore

--- streaming-host-master/up.sh
+++ streaming-host-master/up.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 docker pull docker:1.13.0-dind
 docker-compose up -d
-cd streaming_host
 git clone https://github.com/direnv/direnv
 cd direnv
 sudo make install
+cd streaming_host
 direnv allow
 docker-compose up -d
+cd ../
